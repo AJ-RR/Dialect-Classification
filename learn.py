07144211a261f62c4a6974674d2c93ed92c03a80
pratyush48 <pratyushnandi48@gmail.com>
2019-11-12 13:13:26 +0530

    Added the Loop function

--- learn.py
+++ learn.py
@@ -3,6 +3,8 @@ import pandas as pd
 from sklearn import linear_model
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn import svm
+from sklearn import mixture
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
@@ -27,44 +29,84 @@ def read_data():
  X = data.iloc[:,1:-1]
  y = data.iloc[:,-1]
  return np.array(X), np.array(y)
+	
+def loop(X, y, dialect_list):
+  avg = []
+  for i in range(50):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.1, shuffle = True)
+    avg.append(classify(X_train, y_train, X_test, y_test, dialect_list))
+  SVM_acc = []
+  KNN_acc = []
+  LR_acc = []
+  for i in  range(50):
+    LR_acc.append(avg[i][0])
+    KNN_acc.append(avg[i][1])
+    SVM_acc.append(avg[1][2])
+  
+  SVM_avg = sum(SVM_acc)/len(SVM_acc)
+  KNN_avg = sum(KNN_acc)/len(KNN_acc)
+  LR_avg = sum(LR_acc)/len(LR_acc)
+  
+  print('SVM Accuracy: ', SVM_avg)
 
+  print('Logistic Regression Accuracy: ', LR_avg)
+
+  print('K-Nearest Neighbour Accuracy', KNN_avg)
 
 def classify(X_train, y_train, X_test, y_test, dialect_list):
 
+	scaler = StandardScaler()
+	scaler.fit(X_train)
+	X_train = scaler.transform(X_train)
+	X_test = scaler.transform(X_test)
+	acc = []
 	# Logistic Regression classifier
 
 	logistic_classifier = linear_model.logistic.LogisticRegression()
 	logistic_classifier.fit(X_train, y_train)
 	logistic_predictions = logistic_classifier.predict(X_test)
 	logistic_accuracy = accuracy_score(y_test, logistic_predictions)
+	acc.append(logistic_accuracy)
 	logistic_cm = confusion_matrix(y_test, logistic_predictions)
-	print("logistic accuracy = " + str(logistic_accuracy))
-	print("logistic_cm:")
-	print(logistic_cm)
+	#print("logistic accuracy = " + str(logistic_accuracy))
+	#print("logistic_cm:")
+	#print(logistic_cm)
 
 	# K-Nearest neighbour classifier
 
-	knn_classifier = KNeighborsClassifier()
+	knn_classifier = KNeighborsClassifier(n_neighbors = 3)
 	knn_classifier.fit(X_train, y_train)
 	knn_predictions = knn_classifier.predict(X_test)
 	knn_accuracy = accuracy_score(y_test, knn_predictions)
+	acc.append(knn_accuracy)
 	knn_cm = confusion_matrix(y_test, knn_predictions)
-	print("knn accuracy = " + str(knn_accuracy))
-	print("knn_cm:")
-	print(knn_cm)
+	#print("knn accuracy = " + str(knn_accuracy))
+	#print("knn_cm:")
+	#print(knn_cm)
 
 	# SVM
 
-	svm_classifier = svm.SVC()
+	svm_classifier = svm.SVC(kernel='linear')
 	svm_classifier.fit(X_train, y_train)
 	svm_predictions = svm_classifier.predict(X_test)
 	svm_accuracy = accuracy_score(y_test, svm_predictions)
+	acc.append(svm_accuracy)
 	svm_cm = confusion_matrix(y_test, svm_predictions)
-	print("svm accuracy = " + str(svm_accuracy))
-	print("svm_cm:")
-	print(svm_cm)
+	#print("svm accuracy = " + str(svm_accuracy))
+	#print("svm_cm:")
+	#print(svm_cm)
 
+	#GMM
+	'''gmm_classifier = mixture.GMM(n_components = 9)
+	gmm_classifier.fit(X_train, y_train)
+	gmm_predictions = gmm_classifier.predict(X_test)
+	gmm_accuracy = accuracy_score(y_test, gmm_predictions)
+	gmm_cm = confusion_matrix(y_test, gmm_predictions)
+	print("gmm accuracy = " + str(gmm_accuracy))
+	print("gmm_cm:")
+	print(gmm_cm)'''
 
+	return acc
 
 
 def main():
@@ -74,9 +116,9 @@ def main():
 
  X, y = read_data()
 
- X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.10, shuffle = True)
- classify(X_train, y_train, X_test, y_test, dialect_list)
-
+ #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.1, shuffle = True)
+ #classify(X_train, y_train, X_test, y_test, dialect_list)
+ loop(X, y,dialect_list)
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -9,6 +9,8 @@ from python_speech_features import mfcc
 from python_speech_features import logfbank
 from python_speech_features import delta
 from scipy import stats
+from scipy import signal,fftpack
+import matplotlib.pyplot as plt
 
 DIALECT_DIR = "data"
 DIALECT_LIST = [ "IDR1","IDR2","IDR3","IDR4","IDR5","IDR6","IDR7","IDR8","IDR9"]
@@ -24,10 +26,10 @@ def create_dataset(dialect_list, base_dir):
 
 
                      sampling_rate, song_array = scipy.io.wavfile.read(fn)
-                     print(sampling_rate)
+                     #print(sampling_rate)
                      # song_array = psf.sigproc.preemphasis(song_array)
                      ceps = mfcc(song_array)
-                     print(ceps.shape)
+                     #print(ceps.shape)
                      bad_indices = np.where(np.isnan(ceps))
                      b = np.where(np.isinf(ceps))
                      ceps[bad_indices] = 0
@@ -43,9 +45,10 @@ def create_dataset(dialect_list, base_dir):
                      # lbank[b] = 0
                      # num_lbanks = len(lbank)
                      # temp_mean_lbank = np.mean(lbank[int(num_lbanks*0/10):int(num_lbanks*10/10)], axis = 0)
-                     temp = np.append(temp_mean_ceps,label)
+                     temp1 = np.append(temp_mean_ceps,label)
 
-                     X.append(temp)
+                     X.append(temp1)
+
      X = np.asarray(X)
      print(X.size)
 
@@ -53,10 +56,35 @@ def create_dataset(dialect_list, base_dir):
      dataset = pd.DataFrame(data = X)
      dataset.to_csv('dataset_new.csv')
 
-
+def plots():
+     fs, audio = scipy.io.wavfile.read("1.wav")
+     time = np.arange(0,len(audio)/fs,1/fs)
+     audio = np.asarray(audio)
+     plt.plot(time, audio)
+     plt.xlabel("Time(s)")
+     plt.ylabel("Amplitude(units)")
+     plt.title("Temporal Analysis")
+     plt.show()
+     ceps = mfcc(audio)
+     ceps = ceps[0]
+     #print(ceps)
+     time = np.arange(0,len(ceps)/fs,1/fs)
+     plt.plot(time, ceps)
+     plt.xlabel("Time(s)")
+     plt.ylabel("Amplitude(units)")
+     plt.title("Cepstrum Analysis")
+     plt.show()
+     F = fftpack.fft(audio)
+     freq = fftpack.fftfreq(len(audio))*fs
+     plt.plot(freq,abs(F))
+     plt.title('Fourier Transform')
+     #plt.xlim(0,1000)
+     plt.show()
 
 def main():
- create_dataset(DIALECT_LIST,DIALECT_DIR)
+ plots()
+ #create_dataset(DIALECT_LIST,DIALECT_DIR)
+ #plots()
 if __name__ == "__main__":
 
 	main()
